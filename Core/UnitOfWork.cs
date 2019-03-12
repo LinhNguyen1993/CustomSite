3d6221e898c31eccb741a9b518f07aad948a0547
linhnguyenn3 <nguyenlinh19932014@gmail.com>
2019-03-12 16:27:26 +0700

    delete sample controller and modify UnitOfWork

--- Core/UnitOfWork.cs
+++ Core/UnitOfWork.cs
@@ -7,17 +7,29 @@ namespace CustomSite.Core
     public class UnitOfWork : IUnitOfWork
     {
         private readonly SiteContext _context;
+        private CategoryRepository _categories;
+        private ProductRepository _products;
 
         public UnitOfWork(SiteContext context)
         {
             _context = context;
-            Categories = new CategoryRepository(_context);
-            Products = new ProductRepository(_context);
         }
 
-        public ICategoryRepository Categories { get; private set; }
+        public ICategoryRepository Categories
+        {
+            get
+            {
+                return _categories ?? (_categories = new CategoryRepository(_context));
+            }
+        }
 
-        public IProductRepository Products { get; private set; }
+        public IProductRepository Products
+        {
+            get
+            {
+                return _products ?? (_products = new ProductRepository(_context));
+            }
+        }
 
         public void SaveChange()
         {
