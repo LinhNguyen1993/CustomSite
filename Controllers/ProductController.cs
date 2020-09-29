7cf4f1298121db8a0ce5c35df049cb6206f6f8c7
Admin <Admin@LAPTOP-EMHOADS0>
2020-09-29 23:53:18 +0700

    commit example for tracking

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 using CustomSite.Core;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace CustomSite.Controllers
 {
@@ -9,16 +11,19 @@ namespace CustomSite.Controllers
     public class ProductController : ControllerBase
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private SiteContext _context;
+        public ProductController(IUnitOfWork unitOfWork, SiteContext context)
         {
             _unitOfWork = unitOfWork;
+            _context = context;
         }
 
         [Authorize]
         [HttpGet("[action]")]
         public IActionResult getProducts()
         {
-            var products = _unitOfWork.Products.GetAll();
+            //var products = _unitOfWork.Products.GetAll();
+            var products = _context.Products.AsNoTracking();
 
             return Ok();
         }
